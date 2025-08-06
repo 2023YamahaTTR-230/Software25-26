79e81ca2edb94f748263baaa8d0168e8be1fe337
MorganD <mtdab007@learner.dvusd.org>
2025-08-06 08:10:36 -0700

    C# Console Application Assignment: Personalized Form Letter Generator

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,16 +1,47 @@
-﻿string Firstname;
-string Lastname;
-string Grade;
-string studentID;
-Console.WriteLine("What is your first name?");
-Firstname = Console.ReadLine();
-Console.WriteLine("What is your last name?");
-Lastname = Console.ReadLine();
-Console.WriteLine("What is your grade?");
-Grade = Console.ReadLine();
-Console.WriteLine("What is your student ID?");
-studentID = Console.ReadLine();
-Console.WriteLine("First name: " + Firstname);
-Console.WriteLine("Last name: " + Lastname);
-Console.WriteLine("Grade: " + Grade);
-Console.WriteLine("Student ID: " + studentID);
+﻿
+// Strings
+
+string recipitentName;
+string yourName;
+string Subject;
+string Task;
+string Date;
+
+// Inital Questions
+
+Console.WriteLine("Enter the Recipient's Full Name:");
+recipitentName = Console.ReadLine();
+Console.WriteLine("Enter Your Full Name (Sender):");
+yourName = Console.ReadLine();
+Console.WriteLine("nter the Subject of the Letter:");
+Subject = Console.ReadLine();
+Console.WriteLine("Enter a specific project or task you are discussing:");
+Task = Console.ReadLine();
+Console.WriteLine("Enter a follow-up date");
+Date = Console.ReadLine();
+
+// Letter
+
+Console.WriteLine();
+Console.WriteLine("Generating your Letter...");
+Console.WriteLine();
+Console.WriteLine("8/6/2025");
+Console.WriteLine();
+Console.WriteLine("To: " + recipitentName);
+Console.WriteLine();
+Console.WriteLine("Dear " + recipitentName + ",");
+Console.WriteLine();
+Console.WriteLine("Subject: " + Subject);
+Console.WriteLine();
+Console.WriteLine("I am writing to you today regarding the " + Task + " that we discussed.");
+Console.WriteLine("I wanted to provide a quick update on its progress.");
+Console.WriteLine();
+Console.WriteLine("We anticipate completing the initial phase by " + Date);
+Console.WriteLine("Please let me know if you have any questions or require further information.");
+Console.WriteLine();
+Console.WriteLine("Sincerly,");
+Console.WriteLine();
+Console.WriteLine(yourName);
+Console.WriteLine();
+Console.WriteLine("--------------------------------------");
+Console.WriteLine("Letter generated successfully. Press any key to exit.");
